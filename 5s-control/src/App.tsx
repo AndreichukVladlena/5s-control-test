e7d122f798c6114c70cb543466c9ed9d031f1380
vlanuka <andreychuk04@mail.ru>
2024-12-31 12:57:38 +0300

    feat: register TasksPage

--- 5s-control/src/App.tsx
+++ 5s-control/src/App.tsx
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Layout from './Layout';
 import IndexPage from './pages/Index/IndexPage';
+import TasksPage from './pages/Tasks/TasksPage';
+
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<IndexPage />} />
+          <Route path="/tasks" element={<TasksPage />} />
         </Route>
     </Routes>
   );
